a3d90d9d017aedaaf1578540c1142b023fe9d530
Jan Scholich <jan@scholich.net>
2018-08-20 16:33:02 +0200

    Basic controller for boats

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'boats/index'
+  get 'boats/new'
+  get 'boats/create'
+  get 'boats/edit'
+  get 'boats/update'
+  get 'boats/delete'
+  get 'boats/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
