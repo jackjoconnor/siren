b517b2fef1a8258356476af089764fef39c832e5
Jan Scholich <jan@scholich.net>
2018-08-21 17:59:06 +0200

    Fix seed

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,10 @@
 class Boat < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  mount_uploader :photo, PhotoUploader
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :capacity, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'faker'
-require "pry"
+Booking.delete_all
+Boat.delete_all
+User.delete_all
 
 10.times do
   User.create(email: Faker::Internet.email, password: Faker::Internet.password(8))
 end
 
 10.times do
-  Boat.create(title: Faker::GameOfThrones.character, description: Faker::BackToTheFuture.quote, user_id: User.last.id, price: rand(50..100), capacity: rand(4..10), photo: "https://images.unsplash.com/photo-1520255870062-bd79d3865de7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bf9f05411ebec1834a079815976e93f3&auto=format&fit=crop&w=2852&q=80")
+  Boat.create(title: Faker::GameOfThrones.character, description: Faker::BackToTheFuture.quote, user_id: User.last.id, price: rand(50..100), capacity: rand(4..10), remote_photo_url: "https://images.unsplash.com/photo-1520255870062-bd79d3865de7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bf9f05411ebec1834a079815976e93f3&auto=format&fit=crop&w=2852&q=80")
 end
 
 10.times do |x|
@@ -24,5 +17,4 @@ end
 user = User.new(email: "test@example.com", password: "123456")
 user.owned_boats << Boat.last(5)
 user.bookings << Booking.create(boat_id: 1, user: user, start_date: Faker::Date.between(500.days.ago, Date.today), end_date: Faker::Date.between(500.days.ago, Date.today))
-user.owned
 user.save
