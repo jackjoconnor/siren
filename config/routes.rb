eff11773995c4d17db075eb73e5a4a6b8ef0015c
Sebastien Danglades <sebastien.danglades@gmail.com>
2018-08-21 11:54:42 +0200

    skeleton & basic design boat-card index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/delete'
-  get 'boats/index'
-  get 'boats/new'
-  get 'boats/create'
-  get 'boats/edit'
-  get 'boats/update'
-  get 'boats/delete'
-  get 'boats/show'
+resources :boats, only: [:index]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
