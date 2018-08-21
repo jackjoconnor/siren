e7594a866d1146f94de9ecc98fea532f9ca320f7
Jan Scholich <jan@scholich.net>
2018-08-21 11:32:20 +0200

    Implement routes to all bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,10 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @boat = Boat.find(params[:boat_id])
+    @user = current_user
+    @booking.boat = @boat
+    @booking.user = @user
     if @booking.save
       redirect_to bookings_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :boats do
     resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
