ff438d00ec34b00d373aa1110e3b4a7fe1546373
Jan Scholich <jan@scholich.net>
2018-08-22 12:25:24 +0200

    validation of dates, error message in simple form and restrictive access to pages if not athenticated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,22 @@
 class Booking < ApplicationRecord
   belongs_to :boat
   belongs_to :user
-  validates :start_date, presence: true, uniqueness: true
+  validate :start_date_cannot_be_in_the_past
+  validate :end_date_cannot_be_earlier_than_start_date
+  validates :start_date, presence: true, uniqueness: { scope: :boat}
   validates :end_date, presence: true, uniqueness: true
+
+  private
+
+  def start_date_cannot_be_in_the_past
+    if !start_date.blank? and start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
+
+  def end_date_cannot_be_earlier_than_start_date
+    if !end_date.blank? and end_date < start_date
+      errors.add(:end_date, "can't be before start_date")
+    end
+  end
 end
