8b5c505498d2cceac59d574e0f3dc79b5d91d7bc
Jan Scholich <jan@scholich.net>
2018-08-23 11:03:56 +0200

    Add lat and long to Boat model (automatic geolocation)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_084729) do
+ActiveRecord::Schema.define(version: 2018_08_23_085223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_08_23_084729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
