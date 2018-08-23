7b4e2fb51d8521a33788aa127089d5b4e9f5745e
julien <jlbdcn@gmail.com>
2018-08-23 13:30:01 +0200

    settings search

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,12 @@
 class Boat < ApplicationRecord
+
   has_many :bookings, dependent: :destroy
   belongs_to :user
+
+  pg_search_scope :search_by_title_and_location,
+    against: [ :title, :location ]
+
+
   mount_uploader :photo, PhotoUploader
   validates :title, presence: true
   validates :description, presence: true
